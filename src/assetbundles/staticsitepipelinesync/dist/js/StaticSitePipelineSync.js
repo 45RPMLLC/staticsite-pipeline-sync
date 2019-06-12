b2d152c6a344ece796f9bf352597647daf643cbd
Paco <paco@45rpm.co>
2019-06-11 20:18:57 -0500

    ADDED JS AND CSS FOR PLUGIN PUBLISH BUTTON Js an CSS for publish button

--- src/assetbundles/staticsitepipelinesync/dist/js/StaticSitePipelineSync.js
+++ src/assetbundles/staticsitepipelinesync/dist/js/StaticSitePipelineSync.js
@@ -9,3 +9,35 @@
  * @package   StaticSitePipelineSync
  * @since     1.0.0
  */
+
+// Funciones asíncronas
+//(async function () {
+
+let $publishButton = document.getElementById('ssps-publish');
+$publishButton.addEventListener('click',  (e) => {
+    e.preventDefault();
+
+    let $loaderSVG = document.getElementById('ssps-loader');
+    $loaderSVG.classList.remove('hide');
+    $loaderSVG.classList.add('show');
+
+    fetch($publishButton.getAttribute('href'))
+        .then(function(response) {
+            $loaderSVG.classList.remove('show');
+            $loaderSVG.classList.add('hide');
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+            console.log(response.json());
+        })
+        .catch(function(err) {
+            $publishButton.classList.remove('show');
+            $publishButton.classList.add('hide');
+            console.log(err);
+            console.log('algo falló');
+        });
+});
+
+//})();
